1b0033044973691fd628d5545bea5cec8aa4ff84
Vandana Garia <gariavandana@gmail.com>
2023-12-30 00:55:36 +0530

    first commit

---
+++ ExpenseTracker/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from ExpenseTracker.models import Exp
+
+
+class stform(forms.ModelForm):
+    class Meta:
+        model=Exp
+        fields=['des','amt','date','pay']
+
+
